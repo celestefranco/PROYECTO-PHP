87867e2613639e316414cddc988f7906dde8ff52
luis jimenez <luif1990@hotmail.com>
2023-08-11 23:11:41 -0500

    Update masaConvertir.php

--- Clases/masaConvertir.php
+++ Clases/masaConvertir.php
@@ -9,7 +9,56 @@
    {
        public function conversion($value, $uni, $unf)
        {
-           // codigo
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $value = $_POST["value"];
+            $uni = $_POST["uni"];
+            $unf = $_POST["unf"];
+            
+            // cálculos de conversión 
+            if ($uni == "gramos") {
+                if ($unf == "kilogramos") {
+                    $result = $value / 1000;
+                } elseif ($unf == "libras") {
+                    $result = $value * 0.00220462;
+                }elseif ($unf == "toneladas"){
+                    $result = $value *(1/1000)*(1/1000);
+                }else {
+                    $result = $value;
+                }
+            } elseif ($uni== "kilogramos") {
+                if ($unf == "gramos") {
+                    $result = $value * 1000;
+                } elseif ($unf == "libras") {
+                    $result = $value * 2.20462;
+                }elseif ($unf == "toneladas"){
+                    $result = $value *(1/1000);
+                }else {
+                    $result = $value;
+                }
+            } elseif ($uni == "libras") {
+                if ($unf == "gramos") {
+                    $result = $value * 453.59;
+                } elseif ($unf == "kilogramos") {
+                    $result = $value * 0.453592;
+                }elseif ($unf == "toneladas"){
+                    $result = $value * 0.000453592;
+                }else {
+                    $result = $value;
+                }
+            } elseif ($uni == "toneladas") {
+                if ($unf == "gramos") {
+                    $result = $value * 1000000;
+                } elseif ($unf == "kilogramos") {
+                    $result = $value * 1000;
+                }elseif ($unf == "libras"){
+                    $result = $value * 2204.62;
+                }else {
+                    $result = $value;
+                }
+            }
+            
+           
+        }
        }
    }
    
